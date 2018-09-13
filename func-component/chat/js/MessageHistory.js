5b80ad00e010cedce3602e8d503290ef5572a6fe
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-13 22:18:48 +0300

    Задача 2

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,16 @@
 'use strict';
+MessageHistory.defaultProps = {list: []};
+
+function MessageHistory({list}) {
+    const listMessages = list.map(message => {
+        return (
+            message.type === 'message' && <Message from={message.from} message={message} /> ||
+            message.type === 'response' && <Response from={message.from} message={message} /> ||
+            message.type === 'typing' && <Typing from={message.from} message={message} />
+        );
+    })
+
+
+    return (<ul> {listMessages} </ul>);
+
+}
