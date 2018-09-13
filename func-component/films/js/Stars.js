781c67aeca7cb39020bed260deb5ad8b0a7b1f5d
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-13 21:43:12 +0300

    Задача 1

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,23 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+Stars.defaultProps = {
+  stars: 0
+};
+
+function CalcStars(count) {
+  let StarsArray = []
+  for (let i = 0; i < count; i++) {
+    StarsArray.push(
+      <li><Star/></li>
+    )
+  }
+  return StarsArray;
+}
+
+function Stars({count}) {
+  return (
+    <ul className="card-body-stars u-clearfix">
+      {count >= 1 && count <= 5 && typeof count === 'number' && CalcStars(count)}
+    </ul>
+  )
 }
