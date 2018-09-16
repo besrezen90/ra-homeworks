638130825bf271a77a45ec8f236b0d4f1176253b
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-16 18:49:02 +0300

    Задача 3

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,22 +1,40 @@
 'use strict';
-function AuthForm({onAuth}) {
-const onSubmit = (e) => {
+const AuthForm = (props) => {
+
+  const onSubmit = (event) => {
     event.preventDefault()
-    console.log(event.currentTarget)
-}
+    let form = event.currentTarget;
+    const user = {
+      name: form.elements[0].value,
+      email: form.elements[1].value,
+      password: form.elements[2].value
+    }
+
+    if(props.onAuth) props.onAuth(user)
+    else return
+  }
+
+  const checkMail = (event) => {
+    event.target.value = event.target.value.replace(/[^a-z,A-Z,0-9,_,@,.,-]/g, '');
+  }
+
+  const checkPass = (event) => {
+    event.target.value = event.target.value.replace(/[^a-z,A-Z,0-9, _]/g, '');
+  }
+
 
   return (
-    <form className="ModalForm" action="/404/auth/" method="POST" onAuth={onSubmit}>
+    <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onSubmit}>
       <div className="Input">
         <input type="text" placeholder="Имя"/>
         <label/>
       </div>
       <div className="Input">
-        <input type="email" placeholder="Электронная почта"/>
+        <input type="email" placeholder="Электронная почта" onChange={checkMail}/>
         <label/>
       </div>
       <div className="Input">
-        <input type="password" placeholder="Пароль"/>
+        <input type="password" placeholder="Пароль"  onChange={checkPass}/>
         <label/>
       </div>
       <button type="submit">
@@ -26,3 +44,4 @@ const onSubmit = (e) => {
     </form>
   )
 }
+
