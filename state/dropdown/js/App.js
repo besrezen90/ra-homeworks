ec2aeae199c757fb06c187b811b191fcfa23b37a
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-19 21:38:53 +0300

    Задача 3

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,34 +1,40 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
-      active: this.props.options[0],
-      open: false
-    };
-  },
-  handleChange: function (option) {
+'use strict'
+
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      open: false,
+      active: this.props.options[0]
+    }
+    this.options = !this.props.options && typeof this.props.options == 'object' ? [] : this.props.options
+  }
+
+  handleOpenMenu = () => {
     this.setState({
-      active: option
-    });
-  },
-  toggleOpen: function () {
-    this.setState({
-      open: !this.state.open
-    });
-  },
-  render: function () {
+      open: this.state.open === false ? true : false
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({active: e})
+  }
+
+
+  render() {
+
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
-          <button className={"btn"} onClick={this.toggleOpen} >
+          <button className={"btn"} onClick={this.handleOpenMenu} >
             <span>Account Settings</span>
             <i className="material-icons">public</i>
           </button>
+          {/* <List options={this.options} /> */}
           <ul className="dropdown">
-            {this.props.options.map((option, i) => (
+            {this.options.map((option, i) => (
               <li
+                ket={i}
                 className={option === this.state.active ? "active" : ""}
                 onClick={() => this.handleChange(option)} >
                   <a href="#">{option}</a>
@@ -37,6 +43,6 @@ var App = React.createClass({
           </ul>
         </div>
       </div>
-    );
+    )
   }
-});
+}
