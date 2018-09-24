a29b6104df95f54927bc8d419b21e9fd3af7ffd6
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-24 21:11:44 +0300

    Задача 2

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,14 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
-};
+
+class SearchBox extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+
+    handleFilter = (event) => {
+        this.props.filterBooks(event.currentTarget.value)
+    }
+
+    render() {
+        return <input onChange={this.handleFilter} type="text" placeholder="Поиск по названию или автору" value={this.props.value}/>
+    }
+}
