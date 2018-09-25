b7bb5e5acc2814a056b548da6bcffa2b9427ee52
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-25 21:19:37 +0300

    Задача 3

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,22 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
+const FontSelector = ({fonts, selected, onSelect}) => {
+    const getTypeFont = (event) => {
+        fonts.filter(font => font.name === event.target.name ? onSelect(font) : false)
+    }
+    
+    const Fonts = fonts.map((font, idx) => {
+        return (
+            <div key={idx} className="grid center font-item">
+              <input type="radio" name={font.name} defaultValue={font.name} id={font.name} onChange={getTypeFont} checked={selected && selected.name === font.name}/>
+              <label htmlFor={font.name} className="grid-1">
+                <PictureFont text={'react'} path={font.path}/>
+              </label>
+            </div>
+        )
+    })
+
     return (
         <div className="font-picker">
-            Выберите шрифт
-        </div>
+            {Fonts}
+        </div>   
     )
 };
--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,12 @@
 const TextRenderLine = ({value, onChange}) => {
+	
+	const handleTextRender = (event) => {
+		onChange(event.target.value)
+	}
+
 	return (
 		<div className="type-text">
-			Наберите текст
+		  <textarea name="text" id="font-text" cols={30} rows={2} placeholder="Введите текст для футболки" defaultValue={""} onChange={handleTextRender} value={value}/>
 		</div>
 	);
 };
