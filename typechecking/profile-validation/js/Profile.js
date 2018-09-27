f87d67563534b30874da359eb87c4a33dfa76fd7
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-27 21:18:16 +0300

    Задача 3

--- typechecking/profile-validation/js/Profile.js
+++ typechecking/profile-validation/js/Profile.js
@@ -13,6 +13,50 @@ const imageStyle = {
   height: '200px'
 };
 
+const createMonth = (num) => {
+  num++
+  if(num < 10) return '0' + num;
+  return num
+}
+
+const createDate = (num) => {
+  if(num < 10) return '0' + num;
+  return num
+}
+
+const urlPropType = (props, propName, componentName) => {
+    let url = props[propName];
+    const r = new RegExp('https://vk.com/(id[0-9]+|[A-Za-z0-9_-]+)');
+    let isUrl = (typeof url === 'string') && r.test(url);
+    
+    if(!isUrl) {
+      return new Error(`Неверный параметр ${propName} в компоненте
+        ${componentName}: параметр должен быть url-адресом`);
+    }
+    
+    return null;
+}
+
+const birthdayPropType = (props, propName, componentName) => {
+  let birthday = props[propName];
+  let isBirthday = (typeof birthday === 'string') && /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(birthday);
+
+  const date = new Date().getFullYear() + '-' + createMonth(new Date().getMonth()) + '-' + createDate(new Date().getDate())
+
+  if(!isBirthday) {
+    return new Error(`Неверный параметр ${propName} в компоненте
+      ${componentName}: параметр должен быть датой в формате YYYY-MM-DD`);
+  }
+  if(birthday > date) {
+    return new Error(`Неверный параметр ${propName} в компоненте
+      ${componentName}: параметр не может быть больше ${date}`);
+  }
+  return null;
+}
+
+
+
+
 const Profile = props => {
   return (
     <div className="col-md-4 text-center" style={{marginBottom: '10px'}}>
@@ -27,3 +71,18 @@ const Profile = props => {
     </div>
   );
 };
+
+
+Profile.defaultProps = {
+  img: './images/profile.jpg',
+  birthday: '1988-03-21'
+}
+
+Profile.propTypes = {
+  first_name: PropTypes.string,
+  last_name: PropTypes.string,
+  img: PropTypes.string,
+  url: urlPropType,
+  birthday: birthdayPropType
+}
+
