bf903d7e69c05d9e0b8b11b74aa03de4b9a105c8
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-30 11:05:33 +0300

    Задача 1

---
+++ routing/menu/js/Menu.js
@@ -0,0 +1,24 @@
+class Menu extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <nav className="menu">
+              <NavLink exact to="/" className="menu__item" activeClassName='menu__item-active'>Главная</NavLink>
+              <NavLink exact to="/drift" className="menu__item" activeClassName='menu__item-active'>Дрифт-такси</NavLink>
+              <NavLink exact to="/timeattack" className="menu__item" activeClassName='menu__item-active'>Time Attack</NavLink>
+              <NavLink exact to="/forza" className="menu__item" activeClassName='menu__item-active'>Forza Karting</NavLink>
+            </nav>
+        )
+    }
+}
+
+
+
+
+
+
+
+
