5a85def8643dbb92138f61eee9c0a9c7eb1bb95e
Aleksandr Berezin <besrezen90@mail.ru>
2018-09-30 11:14:42 +0300

    Задача 2

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,20 @@
-
+class App extends React.Component {
+    render() {
+      return (
+        <Router>
+          <div className="tabs">
+            <nav className="tabs__items">
+              <NavLink exact to="/" className="tabs__item" activeClassName='tabs__item-active'>Рефераты</NavLink>
+              <NavLink exact to="/essay" className="tabs__item" activeClassName='tabs__item-active'>Криэйтор</NavLink>
+              <NavLink exact to="/fortune" className="tabs__item" activeClassName='tabs__item-active'>Гадалка</NavLink>
+            </nav>
+            <div className="tabs__content">
+              <Route path="/" exact component={Creator} />
+              <Route path="/essay" component={Essay} />
+              <Route path="/fortune" component={Fortune} />
+            </div>
+          </div>
+        </Router>
+      );
+    }
+  }
