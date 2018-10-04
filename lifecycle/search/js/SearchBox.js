7042d000eb61d393e5b42a6536337d3acf9890ae
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-04 14:18:05 +0300

    Задача 1

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -9,11 +9,23 @@ class SearchBox extends React.Component {
     return <SearchBoxView fixed={this.state.fixed} />
   }
 
+  componentDidMount() {
+    this.setPosition = this.setPosition.bind(this);
+    window.addEventListener('scroll', this.setPosition)
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.setPosition)
+  }
+
   isFixed() {
-    return undefined;
+    const searchBox = document.querySelector('.search-box'),
+          header = document.querySelector('.header');
+    return searchBox.getBoundingClientRect().top <= 0 && searchBox.getBoundingClientRect().top > header.getBoundingClientRect().bottom;
   }
 
   setPosition() {
-    return undefined;
+    this.setState({
+      fixed: this.isFixed() 
+    })
   }
 }
