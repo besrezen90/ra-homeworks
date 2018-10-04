78a67b6450eb9107fda122b6c61387234a8308a4
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-04 17:16:46 +0300

    Задача 3

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,9 +1,14 @@
 class Cart extends React.Component {
+  
 
   render() {
     return (
       <CartView {...this.props} />
     );
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.isOpen !== this.props.isOpen || (nextProps.isOpen && nextProps.items.length !== 0)
   }
 
 }
