2b0123706bbd33a8b2436e2ca044bb2a7df3bc53
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-05 16:48:06 +0300

    Задача 2

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -16,7 +16,6 @@ const List = props => {
 function withUpdate(Component) {
     return class extends React.Component {
         render() {
-            console.log(this.props);
             if(this.props.views < 100) return <New> <Component { ...this.props}/> </New>;
             if(this.props.views > 1000) return <Popular> <Component { ...this.props}/> </Popular>;
             return <Component { ...this.props}/>;
