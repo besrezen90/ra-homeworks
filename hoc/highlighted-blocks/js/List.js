81c24d4939a475dd4ee9d42a251c5590c4897bca
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-05 16:47:41 +0300

    Задача 2

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -4,14 +4,25 @@ const List = props => {
     return props.list.map(item => {
         switch (item.type) {
             case 'video':
-                return (
-                    <Video {...item} />
-                );
+                return ( <UpdateVideo { ...item}/>);
 
             case 'article':
-                return (
-                    <Article {...item} />
-                );
+                return ( <UpdateArticle { ...item}/>);
         }
     });
 };
+
+
+function withUpdate(Component) {
+    return class extends React.Component {
+        render() {
+            console.log(this.props);
+            if(this.props.views < 100) return <New> <Component { ...this.props}/> </New>;
+            if(this.props.views > 1000) return <Popular> <Component { ...this.props}/> </Popular>;
+            return <Component { ...this.props}/>;
+        }
+    }
+}
+
+const UpdateVideo = withUpdate(Video);
+const UpdateArticle = withUpdate(Article);
--- hoc/relative-time/js/DateTime.js
+++ hoc/relative-time/js/DateTime.js
@@ -1,7 +1,29 @@
 'use strict';
 
 const DateTime = props => {
-    return (
-        <p className="date">{props.date}</p>
-    )
+  console.log(props)
+  return (
+    <p className="date">{props.date}</p>
+  )
 };
+
+function Logger(Component) {
+    return function(props, ...args) {
+      props.date = createNewDate(props.date);
+      return Component(props, ...args);
+    }
+}
+
+function createNewDate(value) {
+    const today = new Date();
+    const lastDate = new Date(value);
+    const difference = today.getTime() - lastDate.getTime();
+    
+    if (difference < 3600000) return `${(difference / 60000).toFixed()} минут назад`;
+    if (difference < (3600000 * 24)) return `${(difference / 3600000).toFixed()} часов назад`;
+    else return `${(difference / (3600000 * 24)).toFixed()} дней назад`;
+}
+
+
+
+const DateTimePretty = Logger(DateTime)
