0424826db8b53db1eabb3ed9438c4d7a0c1abf93
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-06 13:40:04 +0300

    Задача 3

--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -17,10 +17,47 @@ class App extends React.Component {
     render() {
         return (
             <div id="app">
-                <MonthTable list={this.state.list} />
-                <YearTable list={this.state.list} />
-                <SortTable list={this.state.list} />
+                <MonthTableUpdate list={this.state.list} />
+                <YearTableUpdate list={this.state.list} />
+                <SortTableUpdate list={this.state.list} />
             </div>
         );
     }
 };
+
+function withUpdate(Component, filter){
+    
+    return class extends React.Component {
+        constructor(...args) {
+            super (...args);
+        }
+        
+        sortDate = (arr, fl) => {
+            if(arr.length === 0) return []
+            arr.map(elem => {
+                const date = new Date(elem.date);
+                elem.year = date.getFullYear();
+                elem.month = date.toLocaleString("en-us", {month: "short"});
+            });
+
+            if(fl) return arr.sort(this.dateSort);
+            else return arr;
+        }
+
+        dateSort = (a, b) => {
+            return new Date(a.date).getTime() - new Date(b.date).getTime();
+        }
+
+        render() {
+            const props = {
+                list: this.sortDate(this.props.list, filter)
+            };  
+                       
+            return <Component {...this.props} {...props}/> 
+        }
+    }
+}
+
+const MonthTableUpdate = withUpdate(MonthTable)
+const YearTableUpdate = withUpdate(YearTable)
+const SortTableUpdate = withUpdate(SortTable, true)
