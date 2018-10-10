2acc464ef79040b5735280081f28eca99c5f28dd
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-10 20:50:41 +0300

    Задача 1

--- hoc/relative-time/js/DateTime.js
+++ hoc/relative-time/js/DateTime.js
@@ -7,23 +7,26 @@ const DateTime = props => {
   )
 };
 
-function Logger(Component) {
-    return function(props, ...args) {
-      props.date = createNewDate(props.date);
-      return Component(props, ...args);
-    }
-}
-
-function createNewDate(value) {
-    const today = new Date();
-    const lastDate = new Date(value);
-    const difference = today.getTime() - lastDate.getTime();
+function withLogger(Component) {
+  return class extends React.Component {
     
-    if (difference < 3600000) return `${(difference / 60000).toFixed()} минут назад`;
-    if (difference < (3600000 * 24)) return `${(difference / 3600000).toFixed()} часов назад`;
-    else return `${(difference / (3600000 * 24)).toFixed()} дней назад`;
+    createNewDate = (value) => {
+      const today = new Date();
+      const lastDate = new Date(value);
+      const difference = today.getTime() - lastDate.getTime();
+      
+      if (difference < 3600000) return `${(difference / 60000).toFixed()} минут назад`;
+      if (difference < (3600000 * 24)) return `${(difference / 3600000).toFixed()} часов назад`;
+      else return `${(difference / (3600000 * 24)).toFixed()} дней назад`;
+    }
+
+    render() {
+        const props = {
+          date: this.createNewDate(this.props.date)
+        }
+        return <Component { ...this.props} {...props}/>;
+    }
+  }
 }
 
-
-
-const DateTimePretty = Logger(DateTime)
+const DateTimePretty = withLogger(DateTime)
