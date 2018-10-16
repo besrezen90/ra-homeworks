aa18ecafaaae415f7b119ededd9fbe5394b97b24
Aleksandr Berezin <besrezen90@mail.ru>
2018-10-17 00:01:33 +0300

    Задача 1

--- other-libs/gmaps/js/Map.js
+++ other-libs/gmaps/js/Map.js
@@ -1,10 +1,43 @@
 class Map extends React.Component {
+  constructor(props) {
+    super(props)
+    this.markers = []
+  }
+
+  componentDidMount() {
+    this.map = new google.maps.Map(this.node, {
+      center: {lat: 59.9884404, lng: 30.1954313},
+      zoom: 1
+    })
+  }
+
+  addMarker = (location) => {
+    const marker = new google.maps.Marker({
+      position: location,
+      map: this.map
+    });
+    this.markers.push(marker);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(null);
+    }
+    this.markers = [];
+    if (this.props !== nextProps) {
+      nextProps.points.forEach(el => {
+        const coord = {
+          lat: el.lat,
+          lng: el.lon
+        }
+        this.addMarker(coord)
+      })
+    }
+  }
+  
   render() {
     return (
-      <div>
-        Карта
-        {this.props.points.map(office => <p>{JSON.stringify(office)}</p>)}
-      </div>
-    );
+      <div className="gMaps" ref={node => this.node = node}></div>
+    )
   }
 }
